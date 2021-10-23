9b9d3f5b8199f0c3fb319c788c92e18c35b05698
Jerryljw <70258359+Jerryljw@users.noreply.github.com>
2021-10-23 11:47:03 +0800

    commit 1.3

--- js/bike_combine.js
+++ js/bike_combine.js
@@ -2,147 +2,82 @@ var bike_combine_chartDom = document.getElementById('bike-combine-chart-div');
 var bike_combine_myChart = echarts.init(bike_combine_chartDom);
 var bike_combine_option;
 var bike_combine_app = {};
-const bike_combine_posList = [
-    'left',
-    'right',
-    'top',
-    'bottom',
-    'inside',
-    'insideTop',
-    'insideLeft',
-    'insideRight',
-    'insideBottom',
-    'insideTopLeft',
-    'insideTopRight',
-    'insideBottomLeft',
-    'insideBottomRight'
-];
-bike_combine_app.configParameters = {
-    rotate: {
-        min: -90,
-        max: 90
-    },
-    align: {
-        options: {
-            left: 'left',
-            center: 'center',
-            right: 'right'
-        }
-    },
-    verticalAlign: {
-        options: {
-            top: 'top',
-            middle: 'middle',
-            bottom: 'bottom'
-        }
-    },
-    position: {
-        options: bike_combine_posList.reduce(function (map, pos) {
-            map[pos] = pos;
-            return map;
-        }, {})
-    },
-    distance: {
-        min: 0,
-        max: 100
-    }
-};
-bike_combine_app.config = {
-    rotate: 90,
-    align: 'left',
-    verticalAlign: 'middle',
-    position: 'insideBottom',
-    distance: 15,
-    onChange: function () {
-        const bike_combine_labelOption = {
-            rotate: bike_combine_app.config.rotate,
-            align: bike_combine_app.config.align,
-            verticalAlign: bike_combine_app.config.verticalAlign,
-            position: bike_combine_app.config.position,
-            distance: bike_combine_app.config.distance
-        };
-        myChart.setOption({
-            series: [
-                {
-                    label: bike_combine_labelOption
-                },
-                {
-                    label: bike_combine_labelOption
-                },
-                {
-                    label: bike_combine_labelOption
-                },
-                {
-                    label: bike_combine_labelOption
-                }
-            ]
-        });
-    }
-};
-const bike_combine_labelOption = {
-    show: true,
-    position: bike_combine_app.config.position,
-    distance: bike_combine_app.config.distance,
-    align: bike_combine_app.config.align,
-    verticalAlign: bike_combine_app.config.verticalAlign,
-    rotate: bike_combine_app.config.rotate,
-    formatter: '{c}  {name|{a}}',
-    fontSize: 16,
-    rich: {
-        name: {}
-    }
-};
+
+echarts.registerTransform(ecStat.transform.regression);
+
 bike_combine_option = {
+    dataset: [
+        {
+            source: [
+                [0, 8.1],
+                [1, 10.1],
+                [2, 10.2],
+                [3, 11.4],
+                [4, 13.3],
+                [5, 14.7],
+                [6, 16.7],
+                [7, 16.8],
+                [8, 16.1]
+            ]
+        },
+        {
+            transform: {
+                type: 'ecStat:regression',
+                config: {
+                    method: 'exponential'
+                    // 'end' by default
+                    // formulaOn: 'start'
+                }
+            }
+        }
+    ],
+    title: {
+        text: '2009 - 2017 Bicycles as a percentage of vehicles（%）',
+        subtext:
+            'Travelling into the central city during AM peak period (7am to 10am)',
+        left: 'center'
+    },
     tooltip: {
         trigger: 'axis',
         axisPointer: {
-            type: 'shadow'
+            type: 'cross'
         }
     },
-    legend: {
-        selectedMode:false,
-        data: ['Swanston', 'Albert', 'Footscray', 'Flinders', 'Canning', 'Yarra North', 'Yarra South']
+    xAxis: {
+        splitLine: {
+            lineStyle: {
+                type: 'dashed'
+            }
+        }
     },
-    xAxis: [
-        {
-            type: 'category',
-            axisTick: { show: false },
-            data: ['Swanston', 'Albert', 'Footscray', 'Flinders', 'Canning', 'Yarra North', 'Yarra South']
+    yAxis: {
+        min: 6,
+        splitLine: {
+            lineStyle: {
+                type: 'dashed'
+            }
         }
-    ],
-    yAxis: [
-        {
-            type: 'value'
-        }
-    ],
+    },
     series: [
         {
-            name: '2008',
-            type: 'bar',
-            barGap: 0,
-            label: bike_combine_labelOption,
-            emphasis: {
-                focus: 'series'
-            },
-            data: [414, 182, 768, 982, 690, 1108, 398]
+            name: 'scatter',
+            type: 'scatter',
+            datasetIndex: 0,
+            symbolSize: 20
         },
         {
-            name: '2012',
-            type: 'bar',
-            label: bike_combine_labelOption,
-            emphasis: {
-                focus: 'series'
-            },
-            data: [758, 890, 1247, 1359, 977, 898, 653]
-        },
-        {
-            name: '2013',
-            type: 'bar',
-            label: bike_combine_labelOption,
-            emphasis: {
-                focus: 'series'
-            },
-            data: [1235, 843, 1093, 1864, 1213, 1093, 899]
+            name: 'line',
+            type: 'line',
+            smooth: true,
+            datasetIndex: 1,
+            symbolSize: 0.1,
+            symbol: 'circle',
+            label: { show: true, fontSize: 15 },
+            labelLayout: { dx: -20 },
+            encode: { label: 2, tooltip: 1 },
+            lineStyle: {
+                width: 5
+            }
         }
     ]
 };
--- js/tram_combine2.js
+++ js/tram_combine2.js
@@ -5,82 +5,88 @@ var tram_combine2_option;
 
 tram_combine2_option = {
     title: {
-        text: 'Tram Departures and Population (by time slot)',
-        left: 'center'
-    },
-
+        text: 'Tram Departures & Population (by time slot)',
+        left: 'center',
+        top: 20
+      },
+    
     tooltip: {
         trigger: 'axis'
-    },
-
-    legend: {
+      },
+     
+      legend: {
         top:20,
         data: ['Population', 'Total Departures'],
         selectedMode:false
-    },
-    xAxis: [
+      },
+      legend: {
+        data: ['Population', 'Total Departures']
+      },
+      xAxis: [
         {
-            type: 'category',
-            data: ['6am-10am', '10am-5pm', '5pm-8pm', '8pm-1am'],
-            axisPointer: {
-                type: 'shadow'
-            }
+          type: 'category',
+          data: ['6am-10am', '10am-5pm', '5pm-8pm', '8pm-1am'],
+          axisPointer: {
+            type: 'shadow'
+          }
         }
-    ],
-    yAxis: [
+      ],
+      yAxis: [
         {
-            type: 'value',
-            name: 'Counts (million)',
-            min: 0,
-            max: 15,
-            interval: 3,
-            splitLine: {
-                "show": false
+          type: 'value',
+          name: 'Counts (million)',
+          min: 0,
+          max: 15,
+          interval: 3,
+          splitLine: {   
+              "show": false
             }
         },
         {
-            type: 'value',
-            name: 'Total Departures',
-            min: 0,
-            max: 150,
-            interval: 30,
-            splitLine: {
-                "show": false
+          type: 'value',
+          name: 'Total Departures',
+          min: 0,
+          max: 150,
+          interval: 30,
+          splitLine: {   
+              "show": false
             }
         },
-
-    ],
-    series: [
+        
+      ],
+      series: [
         {
-            name: 'Population',
-            type: 'bar',
-            data: [
-                6.6,
-                7.5,
-                {
-                    value: 10.1,
-                    itemStyle: {
-                        color: '#6A5ACD'
-                    }
-                }, 1.8],
+          name: 'Population',
+          type: 'bar',
+          data: [
+          6.6, 
+          7.5,
+          {
+              value: 10.1,
+              itemStyle: {
+                color: '#6A5ACD'
+              }
+            }, 1.8],
+            
         },
-
+    
         {
-            name: 'Total Departures',
-            type: 'line',
-            yAxisIndex: 1,
-            data: [135, 108, 135, 60],
-            lineStyle:{
-                color:'#483D8B'
-            },
+          name: 'Total Departures',
+          type: 'line',
+          yAxisIndex: 1,
+          data: [135, 108, 135, 60],
+          lineStyle:{
+          color:'#483D8B'
+          },
             itemStyle : {
-                normal :{ color:'#483D8B'}
-            }
+            normal :{ color:'#483D8B'}
+                                }
         }
-    ]
-};
-
+      ]
+    };
+ 
 tram_combine2_myChart.setOption(tram_combine2_option);
+ 
 tram_combine2_chartDom.style.position = 'fixed';
 tram_combine2_chartDom.style.height = '75%';
 tram_combine2_chartDom.style.width = '60%';
